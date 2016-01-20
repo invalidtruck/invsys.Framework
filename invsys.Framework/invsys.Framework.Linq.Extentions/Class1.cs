53a6107a1529cdeb3a08b473fc38cdaa3b1fe84d
Dave Silva <invalidtruck@msn.com>
2016-01-20 12:31:10 -0600

    Add Solution

---
+++ invsys.Framework/invsys.Framework.Linq.Extentions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace invsys.Framework.Linq.Extentions
+{
+    public class Class1
+    {
+    }
+}
